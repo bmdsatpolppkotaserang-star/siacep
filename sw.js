657e94b447e6a3450bf295e1e14e8022edfdcd73
bmdsatpolppkotaserang-star <bmdsatpolppkotaserang@gmail.com>
2026-09-10 14:29:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - SI-ACEP (PWA CACHE MANAGEMENT)
 // =========================================================================
 
-const CACHE_NAME = 'si-acep-cache-v69.4';
+const CACHE_NAME = 'si-acep-cache-v69.5';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
